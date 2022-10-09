34759d252c382e651a2121882f7c26459b5ba342
umanari145 <umanari145@gmail.com>
2022-10-09 18:31:24 +0900

    psqlのドライバー作成と新しいテーブル作成

---
+++ database/migrations/2022_10_09_093000_create_message_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->uuid('id')->comment('UUID');
+            $table->string('file_path', 255)->comment('S3の画像パス');
+            $table->text('content')->nullable()->comment('メッセージ本文');
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
